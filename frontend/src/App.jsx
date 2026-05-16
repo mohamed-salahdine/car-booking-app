9e7dd1d2e45267414999b537a0b00933c6ca521c
SALAHMED <salahmed.official@gmail.com>
2026-05-16 14:50:43 +0100

    Database + Backend + Frontend  setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600 shadow-lg p-6 bg-white rounded-xl">
+        Tailwind v4 is Working! 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
